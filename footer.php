10cb532f70d7f7c5498ef1e1a9546907e4ed3c19
Roselle Sembrano <RoselleSembrano@example.com>
2023-12-14 20:29:25 +0800

    first commit

---
+++ footer.php
@@ -0,0 +1,61 @@
+<footer>
+        <div class="footerInfo">
+            <div class="footerContainer">
+                <div class="footerItem">
+
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Follow Us <span></span></h4>
+                        </div>
+                        <div class="socialItem">
+                            <div class="socialLink facebook">
+                                <i class="fab fa-facebook-f"></i>
+                            </div>
+                            <div class="socialLink instagram">
+                                <i class="fab fa-instagram"></i>
+                            </div>
+                            <div class="socialLink twitter">
+                                <i class="fab fa-twitter"></i>
+                            </div>
+                        </div>
+                    </div>
+<!--  -->
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Service <span></span></h4>
+                        </div>
+                        <div class="socialItem">
+                            <a href="#"> Bus Ticket </a>
+                            <a href="#"> AirConditioning or </a>
+                            <a href="#"> Non-AirConditioning </a>
+                        </div>
+                    </div>
+<!--  -->
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Our Health <span></span></h4>
+                        </div>
+                        <div class="socialItem">
+                            <a href="#"> Bus Care </a>
+                            <a href="#"> AirConditioning or </a>
+                            <a href="#"> Non-AirConditioning </a>
+                            <a href="#"> 
+                        </div>
+                    </div>
+<!--  -->
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Address <span></span></h4>
+                        </div>
+                        <h5 class="addressTitle">San Fernando - Pampanga</h5>
+                        <div class="callInfo">
+                            <h6>Call</h6> <i class="fas fa-arrow-right"></i>
+                            <span>09123456789</span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+
